162232b3decfe1b1d60cbe13405bd998d6cf6fd7
Wiktor Janecki <j.wiktor05@interia.pl>
2019-03-30 13:20:19 +0100

    Add bad drawing images function

--- src/engineTest/mainGameLoop.java
+++ src/engineTest/mainGameLoop.java
@@ -1,6 +1,10 @@
 package engineTest;
 
+import renderEngine.texture;
 import renderEngine.windowManager;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 
@@ -9,52 +13,98 @@ public class mainGameLoop {
 
     private int fpsCount;
 
+    private static final String TITLE = "ENGINE TEST";
+    private static final String VERSION = "0.00";
+
+
+
+    private static final String SPACE = "          ";
+
 
 
     public static void main(String[] args) {
         System.out.println("Game started successfully!");
         new mainGameLoop();
 
+
     }
     private mainGameLoop()
     {
-        long fpsStart = System.currentTimeMillis() , fpsStop;
+        //Timers for fps counter
+        var fpsStart = new AtomicLong(System.currentTimeMillis());
+        long fpsStop;
 
- 
+
+
+
+
+
         //Loading
         windowManager windowManager = new windowManager();
-        windowManager.windowCreate("Engine Test",1280,720);
+        windowManager.windowCreate(TITLE,1280,720);
         glfwSwapInterval(1);
 
         glEnable(GL_TEXTURE_2D);
 
+        texture texture = new texture("til/testTile.png");
+        glViewport(0,0,1280,720);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, 1280, 0, 720, -1, 1);
+        glMatrixMode(GL_MODELVIEW);
 
         while(!glfwWindowShouldClose(windowManager.getWindow()))
         {
             //logic
+
+                //fps counter
                 fpsCount++;
                 fpsStop = System.currentTimeMillis();
-                if(fpsStop-fpsStart >1000)
+                if(fpsStop- fpsStart.get() >1000)
                 {
-                    fpsStart = System.currentTimeMillis();
-                    glfwSetWindowTitle(windowManager.getWindow(),"GameEngine Test     FPS : " +Integer.toString(fpsCount));
+                    fpsStart.set(System.currentTimeMillis());
+                    glfwSetWindowTitle(windowManager.getWindow(),(TITLE + SPACE +"Version : " + VERSION + SPACE + "FPS : " + "" + fpsCount));
                     fpsCount=0;
                 }
-
-
-
-
-
-
+                //fps counter
 
 
             //event
-            glfwPollEvents();
+
+                //close window
+                glfwPollEvents();
 
 
 
             //render
             glClear(GL_COLOR_BUFFER_BIT);
+
+            //
+            // RENDER IMAGES HERE
+            //
+
+
+
+            texture.bind();
+
+            glBegin(GL_QUADS);
+
+                glTexCoord2f(0,0);
+                glVertex2f(0,100);
+
+                glTexCoord2f(1,0);
+                glVertex2f(100,100);
+
+                glTexCoord2f(1,1);
+                glVertex2f(100,0);
+
+                glTexCoord2f(0,1);
+                glVertex2f(0,0);
+
+            glEnd();
+
+
+
 
 
             glfwSwapBuffers(windowManager.getWindow());
---
+++ src/renderEngine/texture.java
@@ -0,0 +1,45 @@
+package renderEngine;
+
+import org.lwjgl.BufferUtils;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.stb.STBImage.stbi_image_free;
+import static org.lwjgl.stb.STBImage.stbi_load;
+
+public class texture {
+
+    private int id;
+
+    public texture(String filename){
+
+
+        int width2;
+        int height2;
+
+        IntBuffer width = BufferUtils.createIntBuffer(1);
+        IntBuffer height = BufferUtils.createIntBuffer(1);
+        IntBuffer comp = BufferUtils.createIntBuffer(1);
+
+        ByteBuffer data = stbi_load("./res/" + filename, width , height, comp , 4);
+
+        id = glGenTextures();
+        width2 = width.get();
+        height2 = height.get();
+
+        glBindTexture(GL_TEXTURE_2D, id);
+
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width2, height2, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        stbi_image_free(data);
+
+    }
+    public void bind()
+    {
+        glBindTexture(GL_TEXTURE_2D,id);
+    }
+}
